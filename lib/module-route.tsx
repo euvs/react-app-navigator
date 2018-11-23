21ef59a3d81c4222c5a43f438f839b12c647c3c0
euvs <euvs.npm@gmail.com>
2018-11-23 16:10:10 +1100

    Added `ModuleLink`

--- lib/module-route.tsx
+++ lib/module-route.tsx
@@ -1,5 +1,6 @@
-import {Switch, withRouter} from 'react-router';
+import {Route, Switch, withRouter} from 'react-router';
 import * as React from 'react';
+import {Link, LinkProps, NavLink} from "react-router-dom";
 
 const ModuleRouteContext = React.createContext('/');
 
@@ -12,6 +13,7 @@ export const ModuleRoot = withRouter(({children, match}) => {
 export interface IWithModuleRootPathProps {
     moduleRootPath: string
 }
+
 export const withModuleRootPath = (C) => (props) => {
     return <ModuleRouteContext.Consumer>{
         (value) => {
@@ -43,3 +45,26 @@ export const ModuleSwitch: React.FC = ({children}) => {
         }
     }</ModuleRouteContext.Consumer>;
 };
+
+export const ModuleLink: React.FC<LinkProps> = (props) => {
+    return <ModuleRouteContext.Consumer>{
+        (value) => {
+            const newTo = `${value}${props.to}`;
+            return <Link {...props} to={newTo}/>;
+        }
+    }
+    </ModuleRouteContext.Consumer>
+};
+
+export const ModuleNavLink: React.FC<LinkProps> = (props) => {
+    return <ModuleRouteContext.Consumer>{
+        (value) => {
+            const newTo = `${value}${props.to}`;
+            return <NavLink {...props} to={newTo}/>;
+        }
+    }
+    </ModuleRouteContext.Consumer>
+};
+
+
+export const ModuleRoute = Route;
