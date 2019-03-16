ffce5ee8aa1aec8cb3fec81dd4c3defcf739211f
euvs <euvs.npm@gmail.com>
2019-03-16 19:35:41 +1100

    Fixed failing unit-tests with jest 23

--- packages/react-app-navigator/jest.config.js
+++ packages/react-app-navigator/jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     collectCoverageFrom: ['lib/**/*.{js,jsx,ts,tsx}'],
-    setupFilesAfterEnv: ['react-testing-library/cleanup-after-each'],
+    setupTestFrameworkScriptFile: 'react-testing-library/cleanup-after-each',
 };
