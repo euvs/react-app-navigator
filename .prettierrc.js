736e584af0de0fed436f9be580f67d87bd627725
euvs <euvs.npm@gmail.com>
2019-03-16 15:18:28 +1100

    Use tabWidth=2 for json files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,18 @@
 module.exports = {
-    trailingComma: "es5",
+    trailingComma: 'es5',
     tabWidth: 4,
     semi: true,
     singleQuote: true,
     printWidth: 160,
     bracketSpacing: true,
     jsxBracketSameLine: false,
-    arrowParens: "always"
+    arrowParens: 'always',
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
