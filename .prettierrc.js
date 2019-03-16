10bdecbfd9867a98363a3c6129a09362f04a5359
euvs <euvs.npm@gmail.com>
2019-03-16 15:07:43 +1100

    Add prettier and husky pre-commit hook

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 160,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: "always"
+};
