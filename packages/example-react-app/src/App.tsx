3b37d2a9e264ad800799590353bc8fe32875db32
euvs <euvs.npm@gmail.com>
2019-03-16 15:14:22 +1100

    Add lint-staged

--- packages/example-react-app/src/App.tsx
+++ packages/example-react-app/src/App.tsx
@@ -1,67 +1,77 @@
-import React, {Component} from 'react';
-import {Route, Switch} from "react-router";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
 
-import {NavLink} from "react-router-dom";
-import UserRouter from "./Users/UserRouter";
-import {ModuleNavLink, ModuleRoot, ModuleRoute, ModuleSwitch} from '@euvs/react-app-navigator';
-
+import { NavLink } from 'react-router-dom';
+import UserRouter from './Users/UserRouter';
+import { ModuleNavLink, ModuleRoot, ModuleRoute, ModuleSwitch } from '@euvs/react-app-navigator';
 
 const TopNav = () => {
-    return <nav className={"nav"}>
-        <NavLink to={"/"} exact>Home</NavLink>
-        <NavLink to={"/admin"}>Admin</NavLink>
-        <NavLink to={"/admin-as-module"}>Admin As Module</NavLink>
-        <NavLink to={"/users"}>Users</NavLink>
-    </nav>
+    return (
+        <nav className={'nav'}>
+            <NavLink to={'/'} exact>
+                Home
+            </NavLink>
+            <NavLink to={'/admin'}>Admin</NavLink>
+            <NavLink to={'/admin-as-module'}>Admin As Module</NavLink>
+            <NavLink to={'/users'}>Users</NavLink>
+        </nav>
+    );
 };
 
 const NotFound = () => <div>"Not Found :("</div>;
-const Home = () => <div className={"segment"}>App Home</div>;
-const AdminHome = () => <div className={"segment"}>Admin home</div>;
+const Home = () => <div className={'segment'}>App Home</div>;
+const AdminHome = () => <div className={'segment'}>Admin home</div>;
 
 // react-router style where paths are absolute
 const Admin = (props) => {
-    return <div>
-        <nav className={"nav-secondary"}>
-            <NavLink to={`${props.match.path}/`} exact>Admin</NavLink>
-            <NavLink to={`${props.match.path}/users`}>Users</NavLink>
-        </nav>
+    return (
+        <div>
+            <nav className={'nav-secondary'}>
+                <NavLink to={`${props.match.path}/`} exact>
+                    Admin
+                </NavLink>
+                <NavLink to={`${props.match.path}/users`}>Users</NavLink>
+            </nav>
 
-        <Switch>
-            <Route path={`${props.match.path}/`} exact component={AdminHome}/>
-            <Route path={`${props.match.path}/users`} component={UserRouter}/>
-        </Switch>
-    </div>
+            <Switch>
+                <Route path={`${props.match.path}/`} exact component={AdminHome} />
+                <Route path={`${props.match.path}/users`} component={UserRouter} />
+            </Switch>
+        </div>
+    );
 };
 
 // module style where paths are relative to module's root
 const AdminAsModule = () => {
-    return <ModuleRoot>
-        <nav className={"nav-secondary"}>
-            <ModuleNavLink to={"/"} exact>Admin</ModuleNavLink>
-            <ModuleNavLink to={"/users"}>Users</ModuleNavLink>
-        </nav>
+    return (
+        <ModuleRoot>
+            <nav className={'nav-secondary'}>
+                <ModuleNavLink to={'/'} exact>
+                    Admin
+                </ModuleNavLink>
+                <ModuleNavLink to={'/users'}>Users</ModuleNavLink>
+            </nav>
 
-        <ModuleSwitch>
-            <ModuleRoute path="/" exact component={AdminHome}/>
-            <ModuleRoute path="/users" component={UserRouter}/>
-        </ModuleSwitch>
-    </ModuleRoot>
+            <ModuleSwitch>
+                <ModuleRoute path="/" exact component={AdminHome} />
+                <ModuleRoute path="/users" component={UserRouter} />
+            </ModuleSwitch>
+        </ModuleRoot>
+    );
 };
-
 
 class App extends Component {
     render() {
         return (
             <div className="app">
-                <TopNav/>
+                <TopNav />
                 <main>
                     <Switch>
-                        <Route path="/" exact component={Home}/>
-                        <Route path="/admin" component={Admin}/>
-                        <Route path="/admin-as-module" component={AdminAsModule}/>
-                        <Route path="/users" component={UserRouter}/>
-                        <Route path="*" component={NotFound}/>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/admin" component={Admin} />
+                        <Route path="/admin-as-module" component={AdminAsModule} />
+                        <Route path="/users" component={UserRouter} />
+                        <Route path="*" component={NotFound} />
                     </Switch>
                 </main>
             </div>
